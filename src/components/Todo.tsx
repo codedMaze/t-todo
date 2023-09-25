266effa110ef6cd234aadbc0a7a8d5a4d9200b4e
Anuoluwa <hernuincredible@gmail.com>
2023-09-25 09:37:44 +0100

    style

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
 
@@ -12,6 +12,20 @@ type Props = {
 };
 
 const STodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState(todo.todo);
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, todo: editTodo } : { ...todo }
+      )
+    );
+    setEdit(false);
+  };
+
   const handleDone = (id: number) => {
     setTodos(
       todos.map((todo) =>
@@ -19,19 +33,38 @@ const STodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
       )
     );
   };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <form className="todos__single">
-      {todo.isDone ? (
+    <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
+      {edit ? (
+        <input
+          className="todos__single--text"
+          type="text"
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+        />
+      ) : todo.isDone ? (
         <s className="todos__single--text">{todo.todo}</s>
       ) : (
         <span className="todos__single--text">{todo.todo}</span>
       )}
 
       <div>
-        <span className="icon">
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
           <AiFillEdit />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
         <span className="icon" onClick={() => handleDone(todo.id)}>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,16 +4,25 @@ import { Todo } from "../model";
 import STodo from "./Todo";
 
 type Props = {
-  todos: Todo[],
-  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>,
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 };
 
 const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
   return (
-    <div className="todos">
-      {todos.map((todo) => (
-        <STodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
-      ))}
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading"> Active Tasks</span>
+        {todos.map((todo) => (
+          <STodo setTodos={setTodos} todo={todo} todos={todos} key={todo.id} />
+        ))}
+      </div>
+      <div className="todos remove">
+        <span className="todos__heading"> Completed Tasks</span>
+        {todos.map((todo) => (
+          <STodo setTodos={setTodos} todo={todo} todos={todos} key={todo.id} />
+        ))}
+      </div>
     </div>
   );
 };
